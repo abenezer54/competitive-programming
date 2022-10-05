36692ba37f0455c356cb05492f0fecbbccaac8ec
Abenezer Mulugeta <111275351+abenezer54@users.noreply.github.com>
2022-10-05 12:42:39 +0300

    Create Sorting the Sentence.py

---
+++ Sorting the Sentence.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        arr = s.split()
+        
+        arr.sort(key=lambda x: int(x[-1]))
+        
+        return " ".join(x[:-1] for x in arr)
