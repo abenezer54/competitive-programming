94356aca4108447251dcb5a8353fde6dbf030425
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-03-25 17:14:37 +0300

    Create Finding the percentage.py

---
+++ Finding the percentage.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+	n = int(input())
+    student_marks = {}
+    for i in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    percent = sum(student_marks[query_name])/3
+    print('%.2f' % percent)
+    
+    
