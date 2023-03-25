724bbec6140c25e1ac82bdc0f3f9953c3f7159fd
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-03-25 07:45:03 +0300

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i**2)
+    #Using while looop
+    # i = 0
+    # while(i < n):
+    #     print(i**2)
+    #     i += 1
+    
