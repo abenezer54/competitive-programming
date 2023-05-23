d334917071df9153eb11cb568c0e706656ebedc7
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-05-23 21:53:29 +0300

    Update 1859. Sorting the Sentence.py

--- Progress-Sheet/1859. Sorting the Sentence.py
+++ Progress-Sheet/1859. Sorting the Sentence.py
@@ -1,12 +1,5 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
         arr = s.split()
-        reversedArr = []
-        for i in arr:
-            reversedArr.append(i[::-1])
-        reversedArr.sort()
-        arr2 = []
-        for i in reversedArr:
-            arr2.append(i[:0:-1])
-        res = " ".join(arr2)  
-        return res
+        sortedArr = sorted(arr, key = lambda x: x[-1])
+        return " ".join(i[:-1] for i in sortedArr)
