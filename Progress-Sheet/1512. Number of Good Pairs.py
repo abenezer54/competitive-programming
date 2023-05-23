5ecf6e0ae2af223592f57879c22e33dded634a24
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-05-23 23:14:29 +0300

    Update 1512. Number of Good Pairs.py

--- Progress-Sheet/1512. Number of Good Pairs.py
+++ Progress-Sheet/1512. Number of Good Pairs.py
@@ -1,15 +1,22 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
+        #Using Counter from Collections and Arithmetic sum to find the number of pairs
+        from collections import Counter
+        count = 0
+        dic = Counter(nums) #counts the number of repitions for each unique keys
+        for i in dic.values():
+            count += (i * (i-1))//2 #formula to find pairs with number of repititons 
+        return count
         #Using Dictionary
-        dic = {}
-        pair = 0
-        for num in nums:
-            if num in dic:
-                pair += dic[num]
-                dic[num] += 1     
-            else:
-                dic[num] = 1
-        return pair
+        # dic = {}
+        # pair = 0
+        # for num in nums:
+        #     if num in dic:
+        #         pair += dic[num]
+        #         dic[num] += 1     
+        #     else:
+        #         dic[num] = 1
+        # return pair
         #Using array itertation
         # count = 0
         # for i in range(len(nums)):
