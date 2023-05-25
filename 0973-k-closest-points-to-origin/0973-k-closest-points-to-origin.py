0c38e641a271036566d0716f88bddf174c2f0ed6
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-05-25 21:10:21 +0300

    Time: 761 ms (89.32%), Space: 22.4 MB (37.36%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -3,17 +3,3 @@ class Solution:
         from math import sqrt
         points = sorted(points, key = lambda x : sqrt(x[0]**2 + x[1]**2))
         return points[:k]
-#         from math import sqrt
-#         distances = []
-#         dic = {}
-#         for point in points:
-#             d = abs(sqrt(pow(point[0], 2) + pow(point[1], 2)))
-#             distances.append(d)
-#         for i in range(len(distances)):
-#             dic[distances[i]] = points[i]
-#         distances.sort()
-        
-#         ans = []
-#         for i in range(k):
-#             ans.append(dic[distances[i]])
-#         return ans
