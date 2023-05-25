96a1bea6a8a5d02948161c7667e3a47b0d951eb4
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-05-25 19:09:21 +0300

    Update 2418. Sort the People.py

--- Progress-Sheet/2418. Sort the People.py
+++ Progress-Sheet/2418. Sort the People.py
@@ -4,7 +4,15 @@ class Solution:
         arr = []
         for i in range(len(heights)):
             dic[heights[i]] = names[i]
+        
+        #Using Bubble Sort
+        # for i in range(len(heights)):
+        #     for j in range(len(heights) - 1):
+        #         if heights[j] < heights[j+1]:
+        #             heights[j], heights[j+1] = heights[j+1], heights[j]
+       
         heights.sort(reverse = True)
+                
         for i in range(len(heights)):
             arr.append(dic[heights[i]])
         return arr
