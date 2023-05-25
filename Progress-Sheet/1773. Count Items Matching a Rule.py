ed0f8714e0937079ad0078705a335eebb11b216a
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-05-25 14:18:37 +0300

    Update 1773. Count Items Matching a Rule.py

--- Progress-Sheet/1773. Count Items Matching a Rule.py
+++ Progress-Sheet/1773. Count Items Matching a Rule.py
@@ -1,4 +1,23 @@
-if ruleKey == "type":
-            for i in range(len(items)):
-                if items[i][0] == ruleValue:
-                    count += 1
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        #Using dictionary
+        count = 0
+        dic = {"type":0, "color":1, "name":2}
+        for item in items:
+            if item[dic[ruleKey]] == ruleValue:
+                count += 1
+        return count
+        # count = 0
+        # if ruleKey == "type":
+        #     for i in range(len(items)):
+        #         if items[i][0] == ruleValue:
+        #             count += 1
+        # if ruleKey == "color":
+        #     for i in range(len(items)):
+        #         if items[i][1] == ruleValue:
+        #             count += 1
+        # if ruleKey == "name":
+        #     for i in range(len(items)):
+        #         if items[i][2] == ruleValue:
+        #             count += 1
+        # return count
