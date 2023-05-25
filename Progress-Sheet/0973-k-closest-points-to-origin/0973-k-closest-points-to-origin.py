4d4b018470b0f72503ab77666ec86114da37e6b6
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-05-25 21:35:23 +0300

    edit code

--- Progress-Sheet/0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ Progress-Sheet/0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,5 +1,21 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        from math import sqrt
-        points = sorted(points, key = lambda x : sqrt(x[0]**2 + x[1]**2))
+        points = sorted(points, key = lambda x : x[0]**2 + x[1]**2)
         return points[:k]
+        # distances = []
+        # dic = defaultdict(list)
+        # for point in points:
+        #     d = pow(point[0], 2) + pow(point[1], 2)
+        #     distances.append(d)
+        # for i in range(len(distances)):
+        #     dic[distances[i]].append(points[i])
+        # distances.sort()
+        # print(dic)
+        # ans = []
+        # i = 0
+        # j = 0
+        # while i < k:
+        #     ans.extend(dic[distances[j]])
+        #     j += 1
+        #     i += len(dic[distances[j]])
+        # return ans
