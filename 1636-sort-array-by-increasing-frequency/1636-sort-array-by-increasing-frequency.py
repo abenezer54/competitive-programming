afa322ef7ce9f3089d0eb5c06c2b36b4e07ad4ee
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-05-26 19:04:28 +0300

    Time: 70 ms (17.99%), Space: 16.5 MB (8.48%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -1,18 +1,8 @@
-from collections import Counter
+# from collections import Counter
 
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
-        dic = Counter(nums)
-        print(dic)
-        arr = []
-        for key, value in dic.items():
-            arr.append([key, value])
-        print(arr)
-        arr.sort(key = lambda x:(x[1], -x[0] ))
-        print(arr)
-        ans = []
-        for i, j in arr:
-            ans.extend([i]*j)
-        return ans
+        return sorted(sorted(nums, reverse=True), key=nums.count)
+
             
         
