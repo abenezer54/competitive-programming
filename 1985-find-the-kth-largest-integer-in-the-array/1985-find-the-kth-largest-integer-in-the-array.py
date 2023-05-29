4465d3555edca94fa4a435b2db31c6155a6cb81e
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-05-29 10:16:09 +0300

    Time: 288 ms (61.91%), Space: 25 MB (34.29%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
@@ -1,13 +1,4 @@
-from functools import cmp_to_key
 class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
-        def mycmp(a, b):
-            if int(a) > int(b):
-                return -1
-            elif int(a) < int(b):
-                return 1
-            else:
-                return 0
-        nums.sort(key = cmp_to_key(mycmp))
-        
-        return nums[k-1]
+        nums = sorted(map(int, nums), reverse=True)
+        return str(nums[k-1])
