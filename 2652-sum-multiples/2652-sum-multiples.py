1395bb5dfcd3b794ccc9024462000620ff61a290
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-05-31 01:28:14 +0300

    Time: 109 ms (11.96%), Space: 16.2 MB (26.47%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.py
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -2,6 +2,10 @@ class Solution:
     def sumOfMultiples(self, n: int) -> int:
         ans = 0
         for i in range(3, n+1):
-            if i %3 == 0 or i % 5== 0 or i % 7 == 0:
+            if i %3 == 0: 
+                ans += i
+            elif  i % 5== 0:
+                ans += i
+            elif i % 7 == 0:
                 ans += i
         return ans
