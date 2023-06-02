a8207fdafc6dc38c1dca19b4f4ff8bfbdb20e06e
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-06-02 13:56:12 +0300

    Time: 58 ms (77.66%), Space: 16.3 MB (26.62%) - LeetHub

--- Progress-Sheet/0125-valid-palindrome/0125-valid-palindrome.py
+++ Progress-Sheet/0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
-        s = s.lower()
-        newString = "".join([char for char in s if char.isalnum()])
-        reversedString = newString[::-1]  
-        ans = False if not newString == reversedString else True
-        
-        return ans
+        arr = [char.lower() for char in s if char.isalnum()]
+        print(arr)
+        left = 0
+        right = len(arr) - 1
+        isPal = True
+        while left <= right:
+            if arr[left] != arr[right]:
+                isPal = False
+            left += 1
+            right -= 1
+        return True if isPal else False
