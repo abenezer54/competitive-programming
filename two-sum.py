531f27df8cda38760142596cb70461a5799ef5cf
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-11-07 15:32:27 +0300

    added two-sum.python

---
+++ two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i, num in enumerate(nums):
+            if not num in dic:
+                dic[target - num] = i
+            else:
+                return [dic[num], i]
