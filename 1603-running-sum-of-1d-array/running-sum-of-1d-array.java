fd4e3469aa0986f97853f996ca32ed2f57659210
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-11-23 08:33:56 +0300

    Time: 0 ms (100.00%) | Memory: 41.8 MB (42.04%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int[] prefix = Arrays.copyOf(nums, nums.length);
         for(int i = 1; i < nums.length; i++){
-            prefix[i] += prefix[i - 1];
+            nums[i] += nums[i - 1];
         }
-        
-        return prefix;       
+
+        return nums;       
     }
 }
