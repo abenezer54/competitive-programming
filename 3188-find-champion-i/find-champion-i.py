d22963a262275932205b8aab2d4c6c3b2f46743a
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-11-27 09:16:16 +0300

    Time: 468 ms (90.20%) | Memory: 16.7 MB (74.12%) - LeetSync

---
+++ 3188-find-champion-i/find-champion-i.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findChampion(self, grid: List[List[int]]) -> int:
+        for i in range(len(grid)):
+            if sum(grid[i]) == len(grid) - 1:
+                return i
