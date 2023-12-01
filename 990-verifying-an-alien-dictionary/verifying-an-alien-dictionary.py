d51eaafc50569e39c07e7f50c89702529f505919
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-12-01 19:31:57 +0300

    Time: 36 ms (93.06%) | Memory: 16.4 MB (36.26%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
@@ -1,15 +1,15 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
+        dic = {order[i]: i for i in range(26)}
         for i in range(len(words) - 1):
             equal = True
-            for j in range(min(len(words[i]), len(words[i+1]))):
-                i1 = order.index(words[i][j])
-                i2 = order.index(words[i+1][j])
-                if i1 != i2:
+            check = zip(words[i], words[i+1])
+            for a, b in check:
+                if dic[a] != dic[b]:
                     equal = False
-                if i1 > i2:
+                if dic[a] > dic[b]:
                     return False
-                elif i1 < i2:
+                elif dic[a] < dic[b]:
                     break   
             if equal and len(words[i]) > len(words[i+1]):
                 return False
