6c3b1feb0fc0c3e60e9d5ce4109fa19573ea6b1f
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-12-12 18:12:37 +0300

    Added solution for Happy Number

---
+++ python-track/happy-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n != 1:
+            if n in seen:
+                return False
+            seen.add(n)
+            n = sum(int(num)**2 for num in str(n))
+
+        return True
+        
