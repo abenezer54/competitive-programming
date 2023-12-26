9a63879639690821b7c19eac6ddbeea4de329f03
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2023-12-26 19:35:00 +0300

    Time: 233 ms (14.22%) | Memory: 18.9 MB (5.01%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -2,15 +2,24 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         n = len(names)
         look = {heights[i]:names[i] for i in range(len(names))}
+        minn = min(heights)
+        maxx = max(heights)
+        
+        count = [0] * (maxx - minn + 1)
         for i in range(n):
-            min_idx = i
-            for j in range(i + 1,n ):
-                if heights[min_idx] < heights[j]:
-                    min_idx = j
-            heights[min_idx], heights[i] = heights[i], heights[min_idx]
+            count[heights[i] - minn] += 1
+
+        new = [0] * n 
+        cur = 0
+        for j in range(len(count)):
+            if count[j]:
+                new[cur] = j + minn
+                cur += count[j]     
+
+        new.reverse()
+        heights = new
 
         for i in range(n):
             names[i] = look[heights[i]]
 
         return names
-        
