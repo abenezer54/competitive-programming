e5243f626d9836fd16168ac984b7e9c33014229f
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-01-07 13:57:18 +0300

    Time: 28 ms (98.49%) | Memory: 17.4 MB (10.24%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,13 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        z = nums.count(0)
-        o = nums.count(1) + z
-
-        for i in range(len(nums)):
-            if i < z:
-                nums[i] = 0
-            elif i < o:
-                nums[i] = 1
+        left = 0
+        right = len(nums) - 1
+        mid = 0
+        while mid <= right:
+            if nums[mid] == 0:
+                nums[mid], nums[left] = nums[left], nums[mid]
+                left += 1
+                mid += 1
+            elif nums[mid] == 2:
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right -= 1
             else:
-                nums[i] = 2
+                mid += 1
+        
+
+        
+        
