8967f960fc5a840c5f6019e4ae235e10baee9fe6
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-01-10 19:14:34 +0300

    Added solution for Maximum Points You Can Obtain from Cards

--- python-track/maximum-points-you-can-obtain-from-cards.py
+++ python-track/maximum-points-you-can-obtain-from-cards.py
@@ -1,19 +1,15 @@
 class Solution:
     def maxScore(self, card: List[int], k: int) -> int:
-        n = len(card)
-        p = [0] * (n + 1)
-        for i in range(n):
-            p[i + 1] = p[i] + card[i]
+        cur = sum(card[:k])
+        ans = cur
+        r = k - 1
+        l = 0
+        while r > -1:
+            cur -= card[r]
+            r -= 1
+            l -= 1
+            cur += card[l]
+            ans = max(ans, cur)
 
-        rs = [0] * (n + 1)
-        for i in range(n-1, -1, -1):
-            rs[i] = rs[i + 1] + card[i]
-        rs.reverse()
-
-        ans = 0
-        for i in range(k + 1):
-            cur_sum = p[i] + rs[k - i]
-            ans = max(ans, cur_sum)
-            
         return ans
         
