8de62620f0cfc24fa6c0f0c10072000b7296abab
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-01-17 17:56:46 +0300

    Added solution for Range Sum Query 2D - Immutable

--- python-track/range-sum-query-2d-immutable.py
+++ python-track/range-sum-query-2d-immutable.py
@@ -10,11 +10,4 @@ class NumMatrix:
         
 
     def sumRegion(self, r1: int, c1: int, r2: int, c2: int) -> int:
-        ans = self.p[r2 + 1][c2 + 1] - self.p[r2 + 1][c1] - self.p[r1][c2 + 1] + self.p[r1][c1]
-        return ans
-        
-
-
-# Your NumMatrix object will be instantiated and called as such:
-# obj = NumMatrix(matrix)
-# param_1 = obj.sumRegion(row1,col1,row2,col2)
+        return self.p[r2 + 1][c2 + 1] - self.p[r2 + 1][c1] - self.p[r1][c2 + 1] + self.p[r1][c1]
