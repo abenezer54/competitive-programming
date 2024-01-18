a6b1dbb160402ea1e738925f0e80ecf5ab50e6bd
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-01-19 00:56:54 +0300

    Time: 683 ms (93.74%) | Memory: 37.4 MB (5.34%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.py
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.py
@@ -1,17 +1,28 @@
 class Solution:
     def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
-        N = len(nums) 
-        ans = [0] * N
-        prefix = [0] * (N + 1)
+        n = len(nums)
+        prefix, suffix, right, left = [0] * n, [0] * n, [0] * n, [0] * n
 
-        for i in range(0, N):
-            prefix[i + 1] = prefix[i] + nums[i]
- 
-        for i, num in enumerate(nums):
-            rightSum = prefix[-1] - prefix[i + 1]
-            leftSum = prefix[i]
+        for i in range(1, n):
+            right[i] =  nums[i] - nums[i - 1]
+            prefix[i] = prefix[i - 1] + right[i]
+            
+        for i in range(n - 2, -1, -1):
+            left[i] =  nums[i + 1] - nums[i]
+            suffix[i] = suffix[i + 1] + left[i]
 
-            ans[i] = abs((i * num) - leftSum)  + abs(rightSum - ((N -i -1)*num))
-
-        return ans
+        left.pop()
+        left.insert(0, 0)
+        cur_left = [0] * n
+        left_total = sum(suffix)
+        for i in range(n - 1, -1, -1):
+            cur_left[i] = left_total
+            left_total -= left[i] * i
         
+        right_total = sum(prefix)
+        ans = [0] * n
+        for i in range(n):
+            right_total -= right[i] * (n - i)
+            ans[i] = right_total + cur_left[i]
+            
+        return ans
