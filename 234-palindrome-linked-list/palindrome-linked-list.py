97adf8a2da116cf9610fde4983d5a354ecd914c2
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-01-24 22:10:19 +0300

    Time: 261 ms (99.69%) | Memory: 27.1 MB (99.96%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -1,37 +1,26 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack = []
-        length = 0
-        curr = head
-        while curr:
-            length += 1
-            curr = curr.next
-        curr = head
-        i = 0
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        right = slow
+        if fast:
+            right = slow.next
 
-        while curr:
-            if length & 1:
-                if i < length//2:
-                    stack.append(curr.val)
-                elif i > length // 2:
-                    if stack and stack[-1] != curr.val:
-                        return False
-                    if stack:
-                        stack.pop()
-            else:
-                if i < length//2:
-                    stack.append(curr.val)
-                else:
-                    if stack and stack[-1] != curr.val:
-                        return False
-                    if stack:
-                        stack.pop()
-            i += 1
-            curr = curr.next
+        prev = None
+        curr = head
+        nxt = None
+        while curr != slow:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        while right and prev:
+            if right.val != prev.val:
+                return False
+            right = right.next
+            prev = prev.next
 
-        return True       
+        return True
+        
