6fa199bccd716279bf91174b40d154c4bcd8afbe
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-02-20 19:07:37 +0300

    Time: 1056 ms (44.05%) | Memory: 61 MB (85.71%) - LeetSync

--- 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.py
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.py
@@ -3,12 +3,12 @@ class Solution:
         tasks.sort(key = lambda task: (-(task[1] - task[0]), task[0]))
         n, ans, cur = len(tasks), 0, 0
 
-        for i in range(n):
-            if  cur < tasks[i][1]:
-                ans +=  tasks[i][1] - cur
-                cur = tasks[i][1] - tasks[i][0]
+        for actual, minimum in tasks:
+            if  cur < minimum:
+                ans +=  minimum - cur
+                cur = minimum - actual
             else:
-                cur -= tasks[i][0]
+                cur -= actual
             
         return ans
         
