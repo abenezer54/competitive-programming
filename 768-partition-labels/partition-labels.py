20d87902e20b7fabacfec450f3f2ba8f55dc2b80
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-02-20 12:04:08 +0300

    Time: 40 ms (69.81%) | Memory: 16.6 MB (72.02%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,16 +1,13 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        def check(dic, count):
-            for k, v in dic.items():
-                if count[k] != v:
-                    return False
-            return True
-            
-        count, cur, n, l = Counter(s), Counter(), len(s), 0
-        ans = []
+        last, n  = Counter(), len(s)
         for i in range(n):
-            cur[s[i]] += 1
-            if check(cur, count):
+            last[s[i]] = i
+
+        l, end, ans = 0, 0, []
+        for i in range(n):
+            end = max(end, last[s[i]])
+            if i == end:
                 ans.append(i - l + 1)
                 l = i + 1
         return ans
