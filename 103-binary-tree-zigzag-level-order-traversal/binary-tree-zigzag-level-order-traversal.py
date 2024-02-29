d8d7be7771ecf7c37d7738b2b727d8ec54e8978d
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-02-29 10:27:56 +0300

    Time: 34 ms (76.70%) | Memory: 16.7 MB (97.11%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -6,38 +6,14 @@
 #         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root: return []
-        ans, cur = [], []
-        prev, nxt = 1, 0
-        direction = 1
-        queue = deque([root])
-        while queue:
-            if direction:
-                popped = queue.popleft()
-                cur.append(popped.val)
-                if popped.left:
-                    queue.append(popped.left)
-                    nxt += 1
-                if popped.right:
-                    queue.append(popped.right)
-                    nxt += 1
-            else:
-                popped = queue.pop()
-                cur.append(popped.val)
-                if popped.right:
-                    queue.appendleft(popped.right)
-                    nxt += 1
-                if popped.left:
-                    queue.appendleft(popped.left)
-                    nxt += 1
-            prev -= 1
-            if prev == 0:
-                ans.append(cur.copy())
-                cur.clear()
-                prev = nxt
-                nxt = 0
-                direction = 1 - direction
-        return ans
+        hashmap = defaultdict(list)
+        def preorder(cur, depth):
+            if not cur:
+                return 0
+            hashmap[depth].append(cur.val)
+            preorder(cur.left, depth + 1)
+            preorder(cur.right, depth + 1)
+        preorder(root, 0)
 
-
+        return [arr if not depth & 1 else arr[::-1] for depth, arr in hashmap.items() ]
         
