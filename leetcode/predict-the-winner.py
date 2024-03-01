61099cda5f7bfe6f74f8c345d880dedde78c8c77
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-03-01 17:20:10 +0300

    Add solution for Predict the Winner

--- leetcode/predict-the-winner.py
+++ leetcode/predict-the-winner.py
@@ -1,32 +1,14 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        l, r = 0, len(nums) - 1
-        def find(player, l, r):
+        def dfs(player, l, r):
             if l == r:
-                if player == 0:
-                    return [nums[l], 0]
-                return [0, nums[l]]
-            if player == 0:
-                left = find(1 - player, l + 1, r)
-                right = find(1 - player, l, r - 1)
+                return nums[l]       
+            left_score = dfs(player + 1, l + 1, r)
+            right_score = dfs(player + 1, l, r - 1)
 
-                if left[0] + nums[l] > right[0] + nums[r]:
-                    left[0] += nums[l]
-                    return left
-                else:
-                    right[0] += nums[r]
-                    return right
-            else:
-                left = find(1 - player, l + 1, r)
-                right = find(1 - player, l, r - 1)
-
-                if left[1] + nums[l] > right[1] + nums[r]:
-                    left[1] += nums[l]
-                    return left
-                else:
-                    right[1] += nums[r]
-                    return right
-        score = find(0, l, r)
-        if score[0] >= score[1]:
-            return True
-        return False
+            if not player & 1:
+                return max(left_score + nums[l], right_score + nums[r])
+            return min(left_score - nums[l], right_score - nums[r])
+            
+        return dfs(0, 0, len(nums) - 1) >= 0
+      
