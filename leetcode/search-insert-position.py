a3741b0da947a14f25b4f200b084a0cc6c5f55f0
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-03-06 15:07:16 +0300

    Add solution for Search Insert Position

--- leetcode/search-insert-position.py
+++ leetcode/search-insert-position.py
@@ -1,6 +1,3 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        idx = bisect_right(nums, target)
-        if idx - 1 >= 0 and nums[idx - 1] == target:
-            return idx - 1
-        return idx
+        return bisect_left(nums, target)
