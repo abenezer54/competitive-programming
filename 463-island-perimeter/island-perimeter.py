21f3e3c34473ecead6e43c5b91e49f0d5bc179d9
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-04-02 18:50:39 +0300

    Time: 496 ms (13.77%) | Memory: 16.9 MB (92.35%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -15,23 +15,12 @@ class Solution:
                 if (valid(nr, nc) and not grid[nr][nc]) or not valid(nr, nc):
                     tot += 1
             return tot
-            
-        vis = set()
+
         ans = 0
         for i in range(n):
             for j in range(m):
                 if grid[i][j]:
                     ans += find_edges(i, j)
-                    stack = [(i, j)]
-                    vis.add((i, j))
-                    while stack:
-                        top = stack.pop()
-                        for r, c in d:
-                            ni = r + top[0]
-                            nj = c + top[1]
-                            if valid(ni, nj) and (ni, nj) not in vis and grid[ni][nj]:
-                                ans += find_edges(ni, nj)
-                                stack.append((ni, nj))
-                                vis.add((ni, nj))
-                    return ans
+                   
+        return ans
    
