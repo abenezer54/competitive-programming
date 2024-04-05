f2504bee10e4ff6803421603d5c94a8cf99c77bb
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-04-05 11:18:50 +0300

    Time: 138 ms (90.38%) | Memory: 17 MB (75.48%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,18 +1,19 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         n = len(graph)
-        vis = [-1] * n
+        col = [-1] * n
         for v in range(n):
-            if vis[v] == -1:
+            if col[v] == -1:
                 stack = [(v, 0)]
+                col[v] = 0
                 while stack:
-                    node, col = stack.pop()
-                    for u in graph[node]:
-                        if vis[u] != -1:
-                            if vis[u] == col:
-                                return False
-                        else:
-                            vis[u] = col ^ 1
-                            stack.append((u, col ^ 1))
-        return True                   
+                    v, c = stack.pop()
+                    for u in graph[v]:
+                        if col[u] == -1:
+                            stack.append((u, 1 - c))
+                            col[u] = 1 - c
+                        elif col[u] == c:
+                            return False
+        return True
+
         
