a2cb9eae4d893af39b614f2cf3ce7dbbccd4d6e1
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-04-08 21:44:38 +0300

    Time: 66 ms (54.55%) | Memory: 17.1 MB (60.67%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.py
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -2,13 +2,12 @@ class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         n = len(rooms)
         vis = [0] * n
-
-        stack = [0]
         vis[0] = 1
-        while stack:
-            top = stack.pop()
-            for child in rooms[top]:
+        que = deque([0])
+        while que:
+            node = que.popleft()
+            for child in rooms[node]:
                 if not vis[child]:
-                    stack.append(child)
+                    que.append(child)
                     vis[child] = 1
         return vis.count(0) == 0
