e1a9e55c92bc49bbc999578dc2e9bcefae5e8be8
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-04-23 21:41:51 +0300

    Time: 194 ms (25.50%) | Memory: 17.2 MB (84.06%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,24 +1,28 @@
 class Solution:
-    def findCircleNum(self, graph: List[List[int]]) -> int:
-        adj = defaultdict(list)
-        n, m = len(graph), len(graph[0])
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        par = list(range(n))
+        rank = [0] * n
+        def find(v):
+            while v != par[v]:
+                v = par[v]
+            return v
         for i in range(n):
-            for j in range(m):
-                if i != j and graph[i][j]:
-                    adj[i].append(j)
-                    
-        vis = [0] * n
+            for j in range(n):
+                if isConnected[i][j]:
+                    ri = find(i)
+                    rj = find(j)
+                    if ri != rj:
+                        if rank[ri] > rank[rj]:
+                            par[rj] = ri
+                        elif rank[ri] < rank[rj]:
+                            par[ri] = rj
+                        else:
+                            par[rj] = ri
+                            rank[ri] += 1
         ans = 0
-        for v in range(n):
-            if not vis[v]:
+        for i in range(n):
+            if i == find(i):
                 ans += 1
-                stack = [v]
-                vis[v] = 1
-                while stack:
-                    top = stack.pop()
-                    for u in adj[top]:
-                        if not vis[u]:
-                            vis[u] = 1
-                            stack.append(u)
+
         return ans
-        
