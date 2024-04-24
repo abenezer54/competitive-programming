d1ef50849e1249cab798910ef63450f10d4cf8e2
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-04-24 11:16:58 +0300

    Time: 37 ms (38.81%) | Memory: 16.5 MB (85.24%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -3,11 +3,11 @@ class Solution:
         if n < 3:
             if n == 2: return 1
             return n
-            
+
         a, b, c = 0, 1, 1
         for i in range(3, n + 1):
-            x = a + b + c
-            a, b, c = b, c, x
+            d = a + b + c
+            a, b, c = b, c, d
 
         return c
         
