8f2b659384a0a8c9c9b560fb53ec0c2f166524df
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-04-28 07:01:28 +0300

    Time: 362 ms (71.90%) | Memory: 26.4 MB (12.97%) - LeetSync

---
+++ 881-loud-and-rich/loud-and-rich.py
@@ -0,0 +1,24 @@
+class Solution:
+    def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
+        n = len(quiet)
+        ans = list(range(n))
+        adj = [[] for _ in range(n)]
+        ind = [0] * n
+        for a, b in richer:
+            adj[a].append(b)
+            ind[b] += 1
+            
+        que = deque()
+        for i in range(n):
+            if  not ind[i]:
+                que.append(i)
+        
+        while que:
+            node = que.popleft()
+            for child in adj[node]:
+                ind[child] -= 1
+                if quiet[ans[child]] > quiet[ans[node]]:
+                    ans[child] = ans[node]
+                if not ind[child]:
+                    que.append(child)
+        return ans
