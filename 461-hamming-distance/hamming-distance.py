0d6781ed5f982965b2a10cf5abcf8df0d976376d
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-05-07 13:16:34 +0300

    Time: 38 ms (34.55%) | Memory: 16.4 MB (89.60%) - LeetSync

--- 461-hamming-distance/hamming-distance.py
+++ 461-hamming-distance/hamming-distance.py
@@ -1,10 +1,4 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        x = format(x, 'b')
-        y = format(y, 'b')
-        ans = 0
-        for i in range(1, min(len(x), len(y)) + 1):
-            if x[-i] != y[-i]:
-                ans += 1
-        return ans + x[:-i].count('1') + y[:-i].count('1')
+        return (x ^ y).bit_count()
         
