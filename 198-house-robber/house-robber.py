07c2cff675962b4a4276433f3f5fed9de7072722
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-05-09 18:32:11 +0300

    Time: 22 ms (99.51%) | Memory: 16.5 MB (44.34%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,10 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        if len(nums) > 2:
-            nums[2] += nums[0]
+        if n > 2: nums[2] += nums[0]
         for i in range(3, n):
-            nums[i] += max(nums[i - 2], nums[i - 3])
-        
+            nums[i] += max(nums[i - 2], nums[i - 3])      
         return max(nums)
         
