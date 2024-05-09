baa0cba3025e30973dedb28afd628fb631badc35
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-05-09 18:44:24 +0300

    Time: 34 ms (72.51%) | Memory: 16.5 MB (44.34%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -4,5 +4,5 @@ class Solution:
         if n > 2: nums[2] += nums[0]
         for i in range(3, n):
             nums[i] += max(nums[i - 2], nums[i - 3])      
-        return max(nums)
+        return max(nums[-1], nums[-2]) if n >  1 else nums[0]
         
