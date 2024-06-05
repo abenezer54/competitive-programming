ff4d3d0fdd3fec2ef5000a2302ab3fdc257ebb3e
Abenezer Mulugeta Asres <111275351+abenezer54@users.noreply.github.com>
2024-06-05 15:30:32 +0300

    Time: 66 ms (19.45%) | Memory: 17.2 MB (87.44%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.py
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -1,9 +1,11 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
+        #map the indexes with its representing bit
         mp = []
         for i, ch in enumerate(s):
             if ch.isalpha():
                 mp.append(i)
+        #create a copy of a string as list
         cpy = list(s)
         ans = []
         for num in range(1 << len(mp)):
